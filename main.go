7b5da2f6f37c975bb07527636c5ee3b4c9fa3753
lin.xii <lin.xii@outlook.com>
2024-03-12 18:34:42 +0800

    feat: add sse API

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
@@ -11,6 +15,24 @@ func main() {
 	})
 	r.GET("/string", func(c *gin.Context) {
 		c.String(200, "is a string")
+	})
+	r.GET("/stream", func(c *gin.Context) {
+		c.Writer.Header().Set("Content-Type", "text/event-stream")
+		c.Writer.Header().Set("Cache-Control", "no-cache")
+		c.Writer.Header().Set("Connection", "keep-alive")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+
+		times := 10
+
+		for {
+			if times <= 0 {
+				return
+			}
+			c.SSEvent("message", times)
+			time.Sleep(1 * time.Second)
+			times--
+		}
+
 	})
 	r.Run(":8099") // listen and serve on
 
