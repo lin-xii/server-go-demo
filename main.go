a3a90fcef45cf16b88201ce45221f7667236429d
lin.xii <lin.xii@outlook.com>
2024-03-12 11:17:24 +0800

    feat: response with string

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ func main() {
 			"message": "hello world by gin",
 		})
 	})
+	r.GET("/hello/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(200, "Hello %s", name)
+	})
 	r.Run(":8099") // listen and serve on
 
 }
