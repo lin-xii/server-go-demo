4fe033c7e77a945266a012d29f89ef019ca5a8c5
lin.xii <lin.xii@outlook.com>
2024-03-12 14:50:03 +0800

    update api response

--- main.go
+++ main.go
@@ -4,14 +4,13 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/hello", func(c *gin.Context) {
+	r.GET("/json", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "hello world by gin",
 		})
 	})
-	r.GET("/hello/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		c.String(200, "Hello %s", name)
+	r.GET("/string", func(c *gin.Context) {
+		c.String(200, "is a string")
 	})
 	r.Run(":8099") // listen and serve on
 
